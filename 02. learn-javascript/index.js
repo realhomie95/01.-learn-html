8a6edcce93f151675fe822dd259a018f2218e71d
realhomie95 <jjmn1797@gmail.com>
2025-09-16 10:27:49 +0900

    변수와 데이터 타입

--- 02. learn-javascript/index.js
+++ 02. learn-javascript/index.js
@@ -9,3 +9,20 @@ console.log("🚀 ~ 변경된 usermbti:", usermbti);
 
 // username = "newhomie"; //오류발생
 // console.log("🚀 ~ 변경된 username:", username);
+
+// 데이터 타입
+const num = 12;
+const str = "텍스트";
+const bool = true; //false
+let empty; //null
+const empty2 = null;
+
+console.log("num", typeof num);
+console.log("str", typeof str);
+console.log("bool", typeof bool);
+console.log("empty", typeof empty);
+
+/* null과 undefined의 차이
+null: 개발가자 의도를 가지고 값이 비어있음을 명시적으로 나타냄
+undefined: 개발자 의도와 상관 없이 값이 할당되지 않음
+*/
