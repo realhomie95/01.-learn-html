8fffb6aec6b0211974512ef608af0b52e704508c
realhomie95 <jjmn1797@gmail.com>
2025-09-16 11:06:58 +0900

    수정

--- 02. learn-javascript/index.js
+++ 02. learn-javascript/index.js
@@ -1,7 +1,8 @@
+// 변수 선언
 const username = "realhomie"; //상수(변할수 없는 값)
 console.log("🚀 ~ username:", username);
 
-let usermbti = "IsTJ"; //변수(변할수 있는 값)
+let usermbti = "ISTJ"; //변수(변할수 있는 값)
 console.log("🚀 ~ usermbti:", usermbti);
 
 usermbti = "ENFP"; //수정
@@ -26,3 +27,37 @@ console.log("empty", typeof empty);
 null: 개발가자 의도를 가지고 값이 비어있음을 명시적으로 나타냄
 undefined: 개발자 의도와 상관 없이 값이 할당되지 않음
 */
+
+console.log("-------------------------------");
+
+// 함수
+
+// 함수 정의/선언
+// function greeting() {
+//   // 문자열 연결
+//   // console.log("반갑습니다.!" + username + "님" + "당신의 MBTI는" + usermbti + "이군요!");
+//   // 백틱
+//   console.log(`반갑습니다.! ${username}님, 당신의 MBTI는 ${usermbti}이군요!`);
+// }
+
+// 화살표 함수
+const greeting = () => {
+  console.log(`반갑습니다.! ${username}님, 당신의 MBTI는 ${usermbti}이군요!`);
+};
+
+// 함수 호출/실행
+greeting();
+
+// 함수의 인자와 매개변수 (파라미터)
+const sum = (num1, num2) => {
+  //실행할 로직
+  const result = num1 + num2;
+  console.log("🚀 ~ sum ~ result:", result);
+};
+sum(3, 5); //8
+
+// sayHello 함수 만들기 (사용자 이름을 받아서 "반갑습니다. 000님"라는 텍스트를 console에 출력하는 기능을 만들기)
+const sayHello = (name) => {
+  console.log(`반갑습니다. ${name}님`);
+};
+sayHello("전종민");
